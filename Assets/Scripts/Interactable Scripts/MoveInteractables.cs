1650bcb6d19449716636fe1080e4d93423a2f3b4
Kyle <kyledc0804@hotmail.com>
2019-08-12 22:28:22 -0700

    Added Movable Object Scripts
    Added movable/interactable objects that can be pushed/pulled by the player such as boxes/crates

---
+++ Assets/Scripts/Interactable Scripts/MoveInteractables.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveInteractables : MonoBehaviour
+{
+    Vector3 newPlayerVector;
+    Vector3 newLocalScale;
+    public LayerMask objectMask;
+    public float moveDistance = 1f;
+
+    GameObject interactableObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Adjustable player transform 
+        newPlayerVector = new Vector3(transform.position.x, transform.position.y / 1.1f, transform.position.z);
+        newLocalScale = transform.localScale;
+
+        Physics2D.queriesStartInColliders = false;
+        RaycastHit2D hit = Physics2D.Raycast(newPlayerVector, Vector2.right * newLocalScale.x, moveDistance, objectMask);
+
+        // If player is hitting the box and key is being pressed 
+        if (hit.collider != null && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Dragging");
+            interactableObject = hit.collider.gameObject;
+
+            interactableObject.GetComponent<FixedJoint2D>().enabled = true;
+            interactableObject.GetComponent<FixedJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
+
+            // Only set this value once to prevent numerous synchronous calls
+            if (interactableObject.GetComponent<MovingObject>().beingPushed == false)
+            {
+                interactableObject.GetComponent<MovingObject>().beingPushed = true;
+            }
+
+        }
+        else if (Input.GetKeyUp(KeyCode.E))
+        {
+            if (interactableObject)
+            {
+                interactableObject.GetComponent<FixedJoint2D>().enabled = false;
+                interactableObject.GetComponent<MovingObject>().beingPushed = false;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(newPlayerVector, (Vector2) newPlayerVector + Vector2.right * newLocalScale.x * moveDistance);
+    }
+}
---
+++ Assets/Scripts/Interactable Scripts/MovingObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    public bool beingPushed;
+    float xPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xPosition = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Make the object move only when necessary
+        if (beingPushed == false)
+        {
+            transform.position = new Vector3(xPosition, transform.position.y);
+        }
+        else
+        {
+            xPosition = transform.position.x;
+        }
+    }
+}
