6c3f7f33947c89b1335eac1ef458f696c704f3f5
Kyle <kyledc0804@hotmail.com>
2019-09-13 20:46:39 -0700

    Added Destructible Animation and New Scene
    Added destroyed animation for destructible objects
    Added new water scene

--- Assets/Scripts/Collectible Scripts/Destructibles.cs
+++ Assets/Scripts/Collectible Scripts/Destructibles.cs
@@ -7,20 +7,41 @@ public class Destructibles : MonoBehaviour
     [SerializeField] public GameObject itemToDrop;
     [SerializeField] public int health = 1;
 
+    private Animator animator;
+    private bool itemDestroyed = false;
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public void Update()
     {
         // Object has been hit/damaged
         if (health <= 0)
         {
-            if (itemToDrop != null)
+            StartCoroutine("DropItem");
+        }
+    }
+
+    IEnumerator DropItem()
+    {
+        if (itemToDrop != null && !itemDestroyed)
+        { 
+            if (animator != null)
             {
-                // Create a copy of the item and detach it from the destructible object
-                Instantiate(itemToDrop, transform.position, Quaternion.identity, transform);
-                transform.DetachChildren();
+                animator.SetBool("IsDestroyed", true);
             }
 
-            Destroy(gameObject);
+            // Create a copy of the item and detach it from the destructible object
+            itemDestroyed = true;
+            Instantiate(itemToDrop, transform.position, Quaternion.identity, transform);
+            transform.DetachChildren();
         }
+
+        yield return new WaitForSeconds(0.5f);
+
+        Destroy(gameObject);
     }
 
 }
